45e10c459e7e47642c2076d22e8c04282ec9a75a
Josel Cioppa <josel.cioppa@gmail.com>
2015-09-23 12:33:48 -0500

    adding Form package

---
+++ config/workbench.php
@@ -0,0 +1,6 @@
+<?php
+
+return array (
+	'name' => 'Josel Cioppa',
+	'email' => 'josel.cioppa@gmail.com'
+);
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,4 +6,17 @@
 		<h1> Welcome Home! </h1>
 	</div>
 
+	{!! Form::open(['url' => '/']) !!}
+
+		<div class = "form-group">
+			{!! Form::label('aasdf', 'asdf') !!} 
+			{!! Form::text('aasdf', null, ['class' => 'form-control']) !!}
+		</div>
+
+		<div class = "form-group">
+			{!! Form::submit('asdf', ['class' => 'btn btn-primary form-control']); !!}	
+		</div>
+
+	{!! Form::close() !!}
+
 @endsection
